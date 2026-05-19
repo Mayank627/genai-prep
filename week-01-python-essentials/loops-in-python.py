0774622ee34e2464992e39a835a3a9e4726c0e7a
Verma <mayaverma@deloitte.com>
2026-05-19 18:27:49 +0530

    Loops in Python

---
+++ week-01-python-essentials/loops-in-python.py
@@ -0,0 +1,31 @@
+#Conditional statements and loops in Python
+
+age = 25
+
+if age < 18:
+    print("Minor")
+
+elif age > 18:
+    print("Adult")
+
+else:
+    print("Exactly 18")
+
+#Loops
+
+#For Loop
+
+skills = ["python", "java", "C++"]
+for skill in skills:
+    print(skill)
+
+for i in range(1, 5):
+    print(i)
+
+#While Loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+    
+
